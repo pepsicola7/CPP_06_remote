f0099558b8c5e12cacb443a713b34afddcac4e2a
Peiqi Li <peli@c2r11p1.42nice.fr>
2025-06-09 18:22:23 +0200

    create

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peli <peli@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/09 16:38:25 by peli              #+#    #+#             */
+/*   Updated: 2025/06/09 16:41:02 by peli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class ScalarConverter
+{
+private:
+    /* data */
+public:
+    ScalarConverter(/* args */);
+    ~ScalarConverter();
+    static convert();
+};
+
+ScalarConverter::ScalarConverter(/* args */)
+{
+}
+
+ScalarConverter::~ScalarConverter()
+{
+}
